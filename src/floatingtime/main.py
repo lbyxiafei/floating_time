83837ceeba9f046c751423d55099d3192be30dd6
Comma Llama <lbyxiafei@gmail.com>
2024-03-16 13:27:49 -0700

    test: set focus back

--- src/floatingtime/main.py
+++ src/floatingtime/main.py
@@ -1,18 +1,28 @@
 import tkinter as tk
 import time
+import pygetwindow as gw
+
 
 def close_window():
     window.destroy()
 
+
 def update_time():
-    current_time = time.strftime('%H:%M:%S')  # Get current time in HH:MM:SS format
+    current_time = time.strftime("%H:%M:%S")  # Get current time
     label.config(text=current_time)
     window.after(1000, update_time)  # Update every second
+
+
+# Get current window
+cw = gw.getActiveWindow()
 
 # Create the main window without title bar and buttons
 window = tk.Tk()
 window.overrideredirect(True)  # Remove title bar and buttons
-window.attributes('-topmost', 1)  # Make the window stay on top
+window.attributes("-topmost", 1)  # Make the window stay on top
+
+# Reset focus back
+cw.activate()
 
 # Calculate the screen width and height
 screen_width = window.winfo_screenwidth()
@@ -23,10 +33,10 @@ window_width = 200
 window_height = 100
 x_pos = screen_width - window_width
 y_pos = 0
-window.geometry(f'{window_width}x{window_height}+{x_pos}+{y_pos}')
+window.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
 
 # Create a label widget with the current time
-label = tk.Label(window, font=('Arial', 24), fg='white', bg='black')
+label = tk.Label(window, font=("Arial", 24), fg="white", bg="black")
 label.pack(fill=tk.BOTH, expand=True)
 
 # Update the time initially and schedule updates every second
